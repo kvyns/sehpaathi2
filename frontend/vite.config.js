f4873776cc8abe57826f934c63243de6e8ea5aba
Kavyansh <kd.kavyansh2003@gmail.com>
2024-11-18 03:28:14 +0530

    fix reloading issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend/', // Ensure this matches your deployment setup
+  base: '/', // Ensure this matches your deployment setup
 });
