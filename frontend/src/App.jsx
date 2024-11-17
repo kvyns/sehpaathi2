0178005627d25176575d10ba7275de32e0ab7309
Raunak Mandil <136892330+raunnieo@users.noreply.github.com>
2024-11-18 02:03:24 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Home from "./Pages/Home/Home";
+import Home from "./pages/Home/Home";
 
 export default function App() {
   return (
